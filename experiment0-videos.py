9ffdac68cb4c685801b3d6dbac74c03987ffb834
glopesdev <glopesdev@users.noreply.github.com>
2021-04-12 01:59:29 +0100

    Add processing pipeline for top and side videos

--- experiment0-videos.py
+++ experiment0-videos.py
@@ -6,7 +6,7 @@ import subprocess
 root = '/ceph/aeon/test2/data'
 output = os.path.expanduser('~/aeon/data/experiment0')
 bonsai = os.path.expanduser('~/bonsai/Bonsai.Player/bin/Debug/net5.0/Bonsai.Player')
-videoworkflow = 'bonsai/videoproc.bonsai'
+videoworkflow = 'bonsai/tracking.bonsai'
 data = aeon.sessiondata(root)
 
 # fill missing data (crash on the 3rd day due to storage overflow)
@@ -20,15 +20,25 @@ data = data[data.id.str.startswith('BAA')]                        # take only pr
 data = aeon.sessionduration(data)                                 # compute session duration
 print(data.groupby('id').apply(lambda g:g[:].drop('id', axis=1))) # print session summary grouped by id
 
+def exportvideo(path, prefix, framerate, start, end):
+    """Exports and analyses a continuous session video segment."""
+    videoclip = aeon.videoclip(root, prefix, start, end)          # get top video clip between start and end
+    videoinfo = '{0}/{1}-Clips.csv'.format(path, prefix)          # get clip segment info file name
+    videoclip.to_csv(videoinfo)                                   # save top video clip segment info
+    subprocess.call([                                             # call bonsai passing all workflow arguments
+        bonsai,
+        videoworkflow,
+        '-p:ChunkFile={0}'.format(videoinfo),
+        '-p:TrackingFile={0}'.format('{0}/{1}.csv'.format(path, prefix)),
+        '-p:VideoFile={0}'.format('{0}/{1}.avi'.format(path, prefix)),
+        '-p:FrameRate={0}'.format(framerate)])
+
 for session in data.itertuples():                                 # for all sessions
     start = session.Index                                         # session start time is session index
     end = start + session.duration                                # end time = start time + duration
     fname = start.floor('s').isoformat().replace(':','-')         # get a path safe representation of time
     path = '{0}/{1}/{2}'.format(output, session.id, fname)        # format the full name of the output folder
     print('Exporting {0}...'.format(path))                        # print progress report
-    os.makedirs(path, exist_ok=True)                              # ensure output path
-    topvideo = aeon.videoclip(root, 'FrameTop', start, end)       # get top video clip between start and end
-    topvideo.to_csv('{0}/FrameTop.csv'.format(path))              # save top video clip segment info
-    subprocess.call([bonsai, videoworkflow])
-    # sidevideo = aeon.videoclip(root, 'FrameSide', start, end)     # get side video clip between start and end
-    # sidevideo.to_csv('{0}/FrameSide.csv'.format(path))
+    os.makedirs(path, exist_ok=True)                              # ensure output 
+    exportvideo(path, 'FrameTop', 50, start, end)                 # track and export top video
+    exportvideo(path, 'FrameSide', 125, start, end)               # track and export side video
